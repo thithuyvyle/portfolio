9da61381ad5bdd3de6ecd2d4ab4a52c7600a3d8a
thithuyvyle <thithuyvyle@hotmail.com>
2025-06-04 21:34:19 +0200

    changement 04: add real portfolio links

--- script.js
+++ script.js
@@ -10,21 +10,38 @@ function scrollToProjets() {
 }
 /* filtrage projets*/
 let projets = [
-    { nom:  { FR: "Blog Voyage", EN: "Travel Blog" }, descriptif:  { FR: "Blog Voyage", EN: "Travel Blog" }, categorie: "site", image: "images/coming-soon.jpg" },
-    { nom: {FR:"Blog Mode" , EN: "Fashion Blog"}, descriptif: {FR:"Blog Mode" , EN: "Fashion Blog"}, categorie: "site", image: "images/coming-soon.jpg" },
-    { nom: {FR:"Application Mobile" , EN: "Mobile App"}, descriptif: {FR:"Application Mobile" , EN: "Mobile App"}, categorie: "site", image: "images/coming-soon.jpg" },
-    { nom: {FR:"À venir" , EN: "Coming Soon"}, descriptif: {FR:"Création Maquette" , EN: "Wireframe Design"}, categorie: "maquette", image: "images/coming-soon.jpg" },
+    {
+        nom: { FR: "Portfolio version 01", EN: "Portfolio version 01" },
+        descriptif: { FR: "Exercice de création de portfolio", EN: "Portfolio creation exercise" },
+        categorie: "site",
+        image: "images/portfolio_version01.jpg",
+        link: "https://thithuyvyle.github.io/portfolio_exercise/"
+    },
+    {
+        nom: { FR: "Maquette Portfolio", EN: "Portfolio Wireframe" },
+        descriptif: { FR: "Maquette du portfolio", EN: "Portfolio wireframe" },
+        categorie: "maquette",
+        image: "images/figma_portfolio.jpg",
+        link: "https://www.figma.com/proto/wfSMNIys0XieCU6YOE8Da1/Descodeuses-projects?node-id=21-56&starting-point-node-id=21%3A56&t=lwAVZfRCTB7peyvv-1"
+    },
+    {
+        nom: { FR: "Maquette BookFlare", EN: "BookFlare Wireframe" },
+        descriptif: { FR: "Maquette d'un site sur un réseau social de livres", EN: "Wireframe of a website for a social book network" },
+        categorie: "maquette",
+        image: "images/figma_bookflare.jpg",
+        link: "https://www.figma.com/proto/0DO21rp0Zp6V2DjXJsJca2/Interface-Persona?node-id=46-116&starting-point-node-id=46%3A116&t=cFXajU68BtEGSCTI-1"
+    },
 ];
 
 let galerie = document.getElementById("galerie");
 function ajouterProjets(filteredProjets, langue) {
-       galerie.innerHTML = "";// Clear the gallery +++
+    galerie.innerHTML = "";// Clear the gallery +++
     filteredProjets.forEach(proj => {
         const div = document.createElement("div");
         div.className = "projet";
         div.innerHTML =
             `<h3>${proj.nom[langue]}</h3>
-    <img src=${proj.image} alt="${proj.nom[langue]}"></img>
+    <a href=${proj.link} target="_blank"><img src=${proj.image} alt="${proj.nom[langue]}"></img></a>
     <p>${proj.descriptif[langue]}</p>`;
         galerie.appendChild(div);
     });
@@ -51,9 +68,9 @@ function showMaquettes() {
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-    if (galerie){
-    showAllProjets();
-} // show all initially
+    if (galerie) {
+        showAllProjets();
+    } // show all initially
 });
 
 
@@ -126,55 +143,55 @@ function showPopup() {
 
 /** localStorage langues **/
 document.addEventListener("DOMContentLoaded", () => {
-function setText(id, value) {
+    function setText(id, value) {
         const element = document.getElementById(id.startsWith("#") ? id.slice(1) : id);
         if (element && typeof value === "string") {
             element.textContent = value;
         }
     }
 
-let traductions = {};
+    let traductions = {};
 
-fetch("lang.json")
-    .then(response => response.json())
-    .then(data => {
-        traductions = data;
-        const langSave = localStorage.getItem("langue") || "FR";
-        appliquerLangue(langSave);
-    });
+    fetch("lang.json")
+        .then(response => response.json())
+        .then(data => {
+            traductions = data;
+            const langSave = localStorage.getItem("langue") || "FR";
+            appliquerLangue(langSave);
+        });
 
-function appliquerLangue(langue) {
-    const trad = traductions[langue];
-    if (!trad) return; // sécurité en cas d'erreur
+    function appliquerLangue(langue) {
+        const trad = traductions[langue];
+        if (!trad) return; // sécurité en cas d'erreur
 
-    setText("titreDev", trad.titreDev);
-    setText("#soustitreDev", trad.soustitreDev);
-    setText("#savoirPlus", trad.savoirPlus);
-    setText("#titreApropos", trad.titreApropos);
-    setText("#paragraphe1", trad.paragraphe1);
-    setText("#myProjects", trad.myProjects);
-    setText("#myCV", trad.myCV);
-    setText("#tous", trad.tous);
-    setText("#sites", trad.sites);
-    setText("#maquettes", trad.maquettes);
-    setText("#info", trad.info);
-    setText("#nomsLabel", trad.nomsLabel);
-    setText("#nameP", trad.nameP)
-    setText("#emailP", trad.emailP);
-    setText("#telLabel", trad.telLabel);
-    setText("#helpYou", trad.helpYou);
-    setText("#textareaP", trad.textareaP);
-    setText("#buttonSend", trad.buttonSend);
-//placeholder
-    if (nameInput) nameInput.placeholder = trad.placeholderName;
-    if (textarea) textarea.placeholder= trad.placeholderTextarea;
+        setText("titreDev", trad.titreDev);
+        setText("#soustitreDev", trad.soustitreDev);
+        setText("#savoirPlus", trad.savoirPlus);
+        setText("#titreApropos", trad.titreApropos);
+        setText("#paragraphe1", trad.paragraphe1);
+        setText("#myProjects", trad.myProjects);
+        setText("#myCV", trad.myCV);
+        setText("#tous", trad.tous);
+        setText("#sites", trad.sites);
+        setText("#maquettes", trad.maquettes);
+        setText("#info", trad.info);
+        setText("#nomsLabel", trad.nomsLabel);
+        setText("#nameP", trad.nameP)
+        setText("#emailP", trad.emailP);
+        setText("#telLabel", trad.telLabel);
+        setText("#helpYou", trad.helpYou);
+        setText("#textareaP", trad.textareaP);
+        setText("#buttonSend", trad.buttonSend);
+        //placeholder
+        if (nameInput) nameInput.placeholder = trad.placeholderName;
+        if (textarea) textarea.placeholder = trad.placeholderTextarea;
 
-    localStorage.setItem("langue", langue); // sauvegarde à chaque appel
-}
+        localStorage.setItem("langue", langue); // sauvegarde à chaque appel
+    }
 
-let boutonFR = document.getElementById("langFR");
-let boutonEN = document.getElementById("langEN");
-boutonFR.addEventListener("click", () => appliquerLangue("FR"));
-boutonEN.addEventListener("click", () => appliquerLangue("EN"));
+    let boutonFR = document.getElementById("langFR");
+    let boutonEN = document.getElementById("langEN");
+    boutonFR.addEventListener("click", () => appliquerLangue("FR"));
+    boutonEN.addEventListener("click", () => appliquerLangue("EN"));
 })
 
